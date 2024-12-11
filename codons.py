a12e9e075d851a2c841c818e475d07c94c7c0896
eliannagrossman <eliannagro@gmail.com>
2024-12-11 22:44:59 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -6,11 +6,12 @@ def create_codon_dict(file_path):
   file.close()
   codon_dict={}
   for row in rows:
+    row.strip()
     row=row.strip().split('\t')
     if len(row)>=3:
       codon,amino=row[0],row[2]
       codon_dict[codon]=amino
-  return codon_dict 
+  return codon_dict
 
 
 
