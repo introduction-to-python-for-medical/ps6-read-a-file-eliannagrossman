07e636cb671159fdc2ce4e5cdfa8cdc2277f1e3b
eliannagrossman <eliannagro@gmail.com>
2024-12-11 22:49:50 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,17 +1,14 @@
 
 def create_codon_dict(file_path):
-  file_path = "/content/data/codons.txt"
-  file = open(file_path)
-  rows = file.readlines()
-  file.close()
-  codon_dict={}
-  for row in rows:
-    row.strip()
-    row=row.strip().split('\t')
-    if len(row)>=3:
-      codon,amino=row[0],row[2]
-      codon_dict[codon]=amino
-  return codon_dict
+  with open(file_path, 'r') as file:
+    rows = file.readlines()
+    codon_dict={}
+    for row in rows:
+      row=row.strip().split('\t')
+      if len(row)>=3:
+        codon,amino=row[0],row[2]
+        codon_dict[codon]=amino
+    return codon_dict 
 
 
 
